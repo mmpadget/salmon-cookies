1b5392012777f8af5ca8f6027703e0d21f98657e
Michael Padget <mpadget@comcast.net>
2017-02-14 18:21:00 -0800

    Prototype sales per day

--- app.js
+++ app.js
@@ -2,32 +2,99 @@
 
 // <######## START NEW TUESDAY CODE ########>
 
-function CookieStore(name, minCustomers, maxCustomers, avgCookies, hourlyCount) {
+function CookieStore(name, minCustomers, maxCustomers, avgCookies, hourlyCount, customers, sales, totalSales) {
   this.name = name; // The name of the cookie store.
   this.minCustomers = minCustomers; // "this" is dynamic and "this" context will change over and over again.
   this.maxCustomers = maxCustomers; // "this" knows what it is depending on the context.
   this.avgCookies = avgCookies; // "this" gets replaced with "pikePlace" below.
   this.hourlyCount = []; // Default empty array. Here, this would be empty every time.
   this.range = maxCustomers - minCustomers;
+  this.customers = customers;
+  this.sales = sales;
+  this.totalSales = 0;
+  this.dailySales = [];
 }
 
-CookieStore.prototype.getAvgCookieCount = function(){
-  console.log('got avg cookie count.');
-  return Math.floor(Math.random() * ((this.range + 1) + this.minCustomers) * this.avgCookies);
-  this.avgCookies;
+// Adam's example works for one table.
+// CookieStore.prototype.getAvgCookieCount = function(){
+//   console.log('got avg cookie count.');
+//   return Math.floor(Math.random() * ((this.range + 1) + this.minCustomers) * this.avgCookies);
+//   this.avgCookies;
+// };
+
+// 1. Randomize customer data: customers.
+// 2. Determine cookies per hour: cookies.
+// 3. Loop through each hour for each store
+
+// Hours of operation
+var hoursOpen = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
+
+// Goal 1: array of all sales each hour during one day.
+CookieStore.prototype.salesPerDay = function() {
+  for (var iHours = 0; iHours < hoursOpen.length; iHours++) {
+    this.dailySales.push(Math.floor(Math.random() * ((this.range + 1) + this.minCustomers) * this.avgCookies));
+  }
 };
 
-// An instance of a cookie store. pikePlace is an instance.
-var firstAndPike = new CookieStore('First and Pike', 23, 65, 6.3);
-firstAndPike.getAvgCookieCount();
+CookieStore.prototype.totalSalesPerDay = function() {
+  // this.totalSales += sales;
+  this.totalSales = 0;
+  for (var i = 0; i < this.dailySales.length; i++) {
+    this.totalSales += this.dailySales[i];
+  }
+};
 
-// Order: >> ('name', minCustomers, maxCustomers, avgCookies, hourlyCount);
+// Goal 2: function that returns the total cookies for the store.
+// function salesPerStore() {
+//   salesPerDay();
+// };
+
+// CookieStore.prototype.getAvgCookieCount = function() {
+//   for (var iHours = 0; iHours < hoursOpen.length; iHours++) {
+//     console.log('got avg cookie count.');
+//     var sales = Math.floor(Math.random() * ((this.range + 1) + this.minCustomers) * this.avgCookies);
+//     this.avgCookies;
+//     // this.customers = getAvgCookieCount(this.maxCustomers, this.minCustomers);
+//     // this.sales = getAvgCookieCount(customers, this.avgCookies);
+//     this.totalSales += sales;
+//     // console.log(hoursOpen[iHours]);
+//     // createElement('li', 'class', 'hourly-sales', hoursOpen[iHours] + ': ' + sales + ' cookies.', elements[iStores]);
+//     // this.hourlySales.push(sales);
+//   }
+//   console.log(this.totalSales);
+// };
+
+/*
+// // Function to populate array Data.
+// function cookieSales(maxCustomers, minCustomers, avgCookies) {
+//   var hourlySales = [];
+//   for (var iHours = 0; iHours < hoursOpen.length; iHours++) {
+//     var customers = customersThisHour(maxCustomers, minCustomers);
+//     ...
+//   }
+// }
+*/
+
+// Order is ('name', minCustomers, maxCustomers, avgCookies, hourlyCount);
+var firstAndPike = new CookieStore('First and Pike', 23, 65, 6.3);
+firstAndPike.salesPerDay();
+firstAndPike.totalSalesPerDay();
+console.log(firstAndPike.totalSales);
 var seaTacAirport = new CookieStore('Seatac Airport', 3, 24, 1.2);
+// firstAndPike.getAvgCookieCount();
 var seattleCenter = new CookieStore('Seattle Center', 11, 38, 2.3);
+// firstAndPike.getAvgCookieCount();
 var capitolHill = new CookieStore('Capitol Hill', 20, 38, 2.3);
+// firstAndPike.getAvgCookieCount();
 var alki = new CookieStore('Alki', 2, 16, 4.6);
+// firstAndPike.getAvgCookieCount();
 
 var stores = [firstAndPike, seattleCenter, capitolHill, alki];
+
+// Start with this: For loop ... this.dailySales = []; ... turned into <td> element.
+// Create heading and footer seperately. Each store needs <thead><th><td><td> <th><td><td> <th><td><td><tfoot>
+
+// --> Send to DOM / HTML.
 
 // Populate into a simple table. Simple for loop: populate two stores into table.
 var tableEl = document.createElement('table'); // tableEl = <table></table> Step 1. Create entire table.
@@ -61,6 +128,93 @@ document.body.appendChild(tableEl); // Step 3.
 // Create a <div>
 var tableDivEl = document.getElementById('table-div');
 tableDivEl.appendChild(tableEl);
+
+// -- > Duplicated Expendible Looping Example
+/*
+//var sectionEl = document.getElementById('Hello');
+//var divEl = document.getElementsByClassName('other'); // Could dynamicallky build this array var domEl = ['pike-store', ...];
+var pikeEl = document.getElementById('pike-store'); // Method. Referenced from the DOM (not a copy).
+// Define each store ...
+var elements = [pikeEl, ...]; // HTML node. ul/li.
+
+//simple element creation. A function declaration (not expression which would be tied to a variable.)
+function createElement(tagType, tagIdentifier, tagIdentifierName, elementContent, node) {
+  var element = document.createElement(tagType); // Method create element. element is HTML node. Creating a new one.
+  element.setAttribute(tagIdentifier, tagIdentifierName); // myCustomId. Id attribute in HTML (pike-store), can set any attribute.
+  element.textContent = elementContent; // Object has a textContent property. Assign parameter. 'Hello User' assigned to node.
+  // See line 26 below. Also above, all properties are functions attached to an object.
+  console.log(element); // < Element node. Empty open and closed <p> tag here.
+  //give child to the DOM. Everything we do goes straight to the DOM. Adding nodes happens to the DOM.
+  node.appendChild(element); // property of an element, not a method because not a function(). Element will be the new child.
+  // If we pass in the wrong argument (string when looking for sectionEl node), we will get a crash, see line 24.
+}
+
+// Debug with elements tab in browser. Could add 'class' in list below or any attribute.
+// createElement here is a test, but will be done later in a loop.
+// createElement('p', 'id', 'addedContent', 'Hello User', sectionEl); // We would call create element. Tag type parameter.
+// Handing in the actual object sectionEl.
+//<p> id = "myCustomId">Hello User</p> // This is the result of the createElement above.
+//createElement('h1', 'class', 'heading-one', 'It lives!', divEl[0]);
+
+// Customers per hour
+function customersThisHour(MaxCustomers, minCustomers) {
+  var range = MaxCustomers - minCustomers;
+  return Math.floor(Math.random() * range) + minCustomers; // round down:((0-1) * range), then + at least that number of people.
+  // Could create a variable with these arguments. All functions implicitly return, but if not specified it's nothing.
+  console.log(); // Will never run because of return above on line 32.
+}
+// var customers = customersThisHour(20, 9); // Example.
+// Don't put returns in for loops because it will only run once. Common to return result after for loop.
+
+// Cookies sold this hour
+// Because of hoisting if this was defined as an function expression/anonymous function equal to a variable.
+// It would only be available from this point on. The variable would be available, but not the function value.
+// Instead this is defined as a function declaration, declaring a function.
+function cookiesSoldThisHour(customers, avgCookies) {
+  return Math.floor(customers * avgCookies);
+}
+
+// // Function to populate array Data.
+// function cookieSales(maxCustomers, minCustomers, avgCookies) {
+//   var hourlySales = [];
+//   for (var iHours = 0; iHours < hoursOpen.length; iHours++) {
+//     var customers = customersThisHour(maxCustomers, minCustomers);
+//     ...
+//   }
+// }
+
+// Hours of operation
+var hoursOpen = ['6am,' ... '8pm'];
+
+// Objects are made up of key-value pairs. Also creating minCustomers as a property.
+var pikeStore = {
+  minCustomers: 23,
+  maxCustomers: 65,
+  avgCookies: 6.3,
+  totalSales: 0,
+  hourlySales: [],
+  cookieSales: function() {
+    for (var iHours = 0; iHours < hoursOpen.length; iHours++) {
+    var customers = customersThisHour(this.maxCustomers, this.minCustomers);
+    var sales = cookiesSoldThisHour(customers, this.avgCookies);
+    this.totalSales += sales;
+    console.log(this.totalSales);
+    // console.log(hoursOpen[iHours]);
+    createElement('li', 'class', 'hourly-sales', hoursOpen[iHours] + ': ' + sales + ' cookies.', elements[iStores]);
+    this.hourlySales.push(sales);
+    }
+  }
+};
+
+// Stores output
+var stores = [pikeStore, airportStore, centerStore, hillStore, alkiStore];
+for (var iStores = 0; ) {
+  stores[iStores].cookieSales();
+  createElement('li,' 'class', 'store-total', ...)
+
+}
+*/
+// -- > End Duplicated Expendible Looping Example
 
 // <######## END NEW TUESDAY CODE ########>
 
